210d9a97fe951bc8dc1d49d822431c94e18a88e0
eeng20 <tul40997@temple.edu>
2024-09-19 14:57:24 -0400

    Implemented functions for TextSizeAdapter

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,44 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val context: Context, private val numberArray: Array<Int>) : BaseAdapter() {
+
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numberArray.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numberArray[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView : TextView
+
+        if (convertView != null) {
+            textView = convertView as TextView
+        } else {
+            textView = TextView(context)
+
+        }
+        textView.text = numberArray[position].toString()
+        textView.setPadding(5, 10, 0, 10)
+        textView.textSize = numberArray[position].toFloat()
+
+        return textView
+
+
     }
+
+    
 
 }
